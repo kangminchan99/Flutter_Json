a63856bc4be508d13683a74347b250d52ac511ff
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-15 21:32:59 +0900

    test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,4 +9,15 @@ class User {
       required this.name,
       required this.email,
       required this.phone});
+
+  // fromJson - json 데이터를 dart 오브젝트로 변환
+  factory User.fromJson(Map<String, dynamic> json) => User(
+      userId: json['userId'],
+      name: json['name'],
+      email: json['email'],
+      phone: json['phone']);
+
+  // toJson - dart오브젝트에서 제이슨 데이터로 변환
+  Map<String, dynamic> toJson() =>
+      {'userid': userId, 'name': name, 'email': email, 'phone': phone};
 }
